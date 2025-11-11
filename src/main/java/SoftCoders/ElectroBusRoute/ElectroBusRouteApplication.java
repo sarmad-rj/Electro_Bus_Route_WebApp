8f6a6000a773d2bfb46fbd91092e41630719dc81
Sarmad-Rj <sarmadrajpoot291@gmail.com>
2025-11-11 18:38:59 +0500

    Initial Commit

---
+++ src/main/java/SoftCoders/ElectroBusRoute/ElectroBusRouteApplication.java
@@ -0,0 +1,13 @@
+package SoftCoders.ElectroBusRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroBusRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroBusRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SoftCoders/ElectroBusRoute/ElectroBusRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package SoftCoders.ElectroBusRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroBusRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
